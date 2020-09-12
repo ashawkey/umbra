a6f9a7f6c3e24cd253e39ee801776fe2a2acd234
ashawkey <963113090@qq.com>
2020-09-12 18:11:04 +0800

    Updates

---
+++ precache-manifest.4f9ca5f4e36044b8e79aa95d6ba2f4a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd90287438684a1d896f5bcd0a74168",
+    "url": "/umbra/index.html"
+  },
+  {
+    "revision": "f597f908a0c8380c1dad",
+    "url": "/umbra/static/css/main.afadfeb2.chunk.css"
+  },
+  {
+    "revision": "bca62bd877f3c8b6fabc",
+    "url": "/umbra/static/js/2.d1ed3061.chunk.js"
+  },
+  {
+    "revision": "7cc9bc71dc193dc16a761d4db5c08eb5",
+    "url": "/umbra/static/js/2.d1ed3061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f597f908a0c8380c1dad",
+    "url": "/umbra/static/js/main.645abc20.chunk.js"
+  },
+  {
+    "revision": "d40dff9ea6d3106c8ee4",
+    "url": "/umbra/static/js/runtime-main.42717f6e.js"
+  }
+]);
